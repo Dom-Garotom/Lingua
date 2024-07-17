c29bca96bf6896ea06673bb0e0faedb35e79c371
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-07-17 17:07:15 -0300

    feat : create a component of header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import Link from "next/link";
+import ButtonDefault from "../buttonDefault/buttonDeafult";
+import Logo from "../../../public/Logo.svg"
+
+function Header (){
+
+    var links = [
+        {
+            id : 1 , 
+            link : "Courses",
+            href : ""
+        },
+        {
+            id : 2 , 
+            link : "About",
+            href : ""
+        },
+        {
+            id : 3 , 
+            link : "Teachers",
+            href : ""
+        },
+        {
+            id : 3 , 
+            link : "Pricing",
+            href : ""
+        },
+        {
+            id : 4 , 
+            link : "Careers",
+            href : ""
+        },
+        {
+            id : 5 , 
+            link : "Blog",
+            href : ""
+        }
+    ]
+
+
+    return(
+        <>
+            <header className="flex justify-between items-center px-10 py-4" >
+                <div>
+                    <Image src={Logo} alt="Logo da empresa Lingua" />
+                </div>
+
+                <nav className="flex flex-row gap-5">
+                    {links.map( item => (
+                        <Link 
+                            key={item.id} 
+                            href={item.href}
+                            className="text-[#666680] text-xl font-normal"
+                        >
+                            {item.link}
+                        </Link>
+                    ))}
+                </nav>
+
+                <ButtonDefault 
+                    bg="bg-white"
+                    content="Get started"
+                    color="text-[#524FD5]"
+                />
+            </header>
+        </>
+    )
+}
+
+export default Header;
